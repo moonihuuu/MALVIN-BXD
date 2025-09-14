2fd009e283fb35e8535cfe25391cbc370f5734d6
moonihuuu <spamsultanx@gmail.com>
2025-09-15 00:00:34 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~4ztC1ZIA#2tX4noFc-XS-I-6-JZtcb1hyYzu_d5KEE0Nm2LjB_cc",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "923000777232", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
